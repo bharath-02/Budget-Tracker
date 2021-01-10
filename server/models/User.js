dc74c14e99f7414c29b0b70214c1b168bbfdb166
bhara <bharathJ222000@gmail.com>
2021-01-10 16:22:17 +0530

    added

---
+++ server/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add some text']
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add your email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    register_date: {
+        type: Date,
+        default: Date.now
+    },
+    // transactions: [{
+    //   type: Schema.Types.ObjectId, 
+    //   ref: 'Transaction', 
+    // }],
+});
+
+UserSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ id: this.id, name: this.name },
+        'myjwt'
+    );
+    return token;
+};
+
+const User = mongoose.model('User', UserSchema);
+exports.User = User;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
-const connectDB = require('./config/db');
 
-dotenv.config({ path: './config/config.env' });
-
-connectDB();
+const NODE_ENV = 'production';
+const MONGO_URI = 'mongodb+srv://Admin:tIpHfdLqYUnoX4UI@cluster0.6j8wc.mongodb.net/budgetTracker?retryWrites=true&w=majority'
 
 // const transactions = require('./routes/transactions');
 
@@ -15,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'development') {
+if (NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
@@ -23,16 +22,24 @@ app.use('/api/transactions', require('./routes/transactions'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 
-if (process.env.NODE_ENV === 'production') {
+if (NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
 
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.log('Error Occured', err);
+    } else {
+        console.log('Database Connected'.cyan.bold);
+    }
+})
+
 app.listen(
     PORT,
     console.log(
-        `Server running in ${process.env.NODE_ENV} mode in port ${PORT}`.blue.bold
+        `Server running in ${NODE_ENV} mode in port ${PORT}`.blue.bold
     )
 );
